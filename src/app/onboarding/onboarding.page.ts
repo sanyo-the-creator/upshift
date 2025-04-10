c458811dd2329d4672db25702ecb4ebbfcee6633
Alexander <sanipoole13@gmail.com>
2025-04-10 13:40:54 +0200

    onboarding

--- src/app/onboarding/onboarding.page.ts
+++ src/app/onboarding/onboarding.page.ts
@@ -1,5 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+
+interface Answer {
+  id: number;
+  answer: string;
+}
+
+interface Question {
+  id: number;
+  question: string;
+  answers: Answer[];
+}
+
 @Component({
   standalone: false,
   selector: 'app-onboarding',
@@ -7,8 +19,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./onboarding.page.scss'],
 })
 export class OnboardingPage implements OnInit {
+  public currentQuestion:Question | null = null;
+  public questionList: Question [] = [
+    {
+      id: 0,
+      question: 'Whatap nigga whattap cuh mohli by sme byt fake zebraci no asi ne, robota normalny zivot neni pre nas asi ne',
+      answers: [
+        {
+          id: 0,
+          answer: 'Good'
+        },
+        {
+          id: 1,
+          answer: 'Sybau'
+        },
+        {
+          id: 2,
+          answer: 'WHAT?'
+        }
+      ]
+    },
+    {
+      id: 1,
+      question: 'Are you the top G?',
+      answers: [
+        {
+          id: 0,
+          answer: 'YASS DADDY'
+        },
+        {
+          id: 1,
+          answer: '??'
+        },
+        {
+          id: 2,
+          answer: 'gay'
+        }
+      ]
+    }
+  ]
 
-  constructor() { }
+  constructor() {
+    this.currentQuestion = this.questionList[0]
+  }
 
   ngOnInit() {
   }
