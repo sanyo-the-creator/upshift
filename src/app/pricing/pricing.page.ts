9c49496ed9c446f107b2a5e81ed1abeb79524264
Alexander <sanipoole13@gmail.com>
2025-04-11 17:10:31 +0200

    pricing

--- src/app/pricing/pricing.page.ts
+++ src/app/pricing/pricing.page.ts
@@ -7,7 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pricing.page.scss'],
 })
 export class PricingPage implements OnInit {
-
+  public pageContent = [
+    {
+      icon: 'lock-open',
+      title: 'Today',
+      text:'Unlock all app features, like group challenges, side quests and more.'
+    },
+    {
+      icon: 'barbell',
+      title: 'In 1 month',
+      text:'You will notice a difference in your productivity and focus.'
+    },
+    {
+      icon: 'diamond',
+      title: 'In 365 days',
+      text:'You will be a different person, with consistent routine, better focus and more energy.'
+    }
+  ]
   constructor() { }
 
   ngOnInit() {
